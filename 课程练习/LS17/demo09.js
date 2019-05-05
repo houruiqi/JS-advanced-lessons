a52a0a9c1c5135751bd51ba60362013f0e50593f
houruiqi <971469259@qq.com>
2019-05-05 20:03:09 +0800

    5月5日

---
+++ 课程练习/LS17/demo09.js
@@ -0,0 +1,54 @@
+/**
+ * Created by qile on 2017/8/14.
+ */
+//JS实现继承的形式 一
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype.__proto__ = Person.prototype;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
+
+
+//JS实现继承的形式 二
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){
+    console.log(this.name);
+};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype = Object.create(Person.prototype);
+// console.log(Person.prototype.constructor); 
+/*
+	ƒ Person(name,age){
+    	this.name = name;
+    	this.age = age;
+	}
+*/
+// console.log(Student.prototype.constructor);
+/*
+	ƒ Person(name,age){
+    	this.name = name;
+    	this.age = age;
+	}
+*/
+Student.prototype.constructor = Student;
+/*
+	ƒ Student(name,age,id){
+    	Person.call(this,name,age);
+    	this.id = id;
+	}
+*/
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
