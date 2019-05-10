f716466d81a4e5afc7330c8ce96ff7ffa031c3e6
houruiqi <971469259@qq.com>
2019-05-10 20:06:12 +0800

    5月10日

---
+++ 课程练习/LS20/demo02.js
@@ -0,0 +1,46 @@
+/**
+ * Created by qile on 2017/10/23.
+ */
+//在控制台上测试，了解两点
+// 一、g全局、i大小写、m换行 修饰符的作用
+// 二、正则对象的两种基本使用方式 1.字符串.字符串方法（正则对象） 2.正则对象.正则方法（字符串）
+var regExp = /ab/i;
+var matchResult = "xxAbcaaBbxyz".match(regExp);
+console.log(matchResult);
+//没有
+
+var regExp = /ab/gi;
+var matchResult = "xxAbcaaBbxyz".match(regExp);
+console.log(matchResult);
+//["Ab", "aB"]
+
+//* 匹配前一个表达式0次或多次。
+var regExp = /a*b/gi; //注意*和.的区别 ，参见在线分析工具 https://regexper.com或https://jex.im/regulex
+var matchResult = "xxAbcaaBbxyz".match(regExp);
+console.log(matchResult);
+//["Ab", "aaB", "b"]
+
+//. 匹配除换行符之外的任何单个字符。
+var regExp = /a.b/gi;//注意*和.的区别 ，参见在线分析工具 https://regexper.com或https://jex.im/regulex
+var matchResult = "xxAbcaaBbxyz".match(regExp);
+console.log(matchResult);
+//["aaB"]
+
+//test初步了解
+var regExp = /a/i;
+console.log(regExp.test("ab"));//true
+console.log(regExp.test("ab"));//true
+console.log(regExp.test("ab"));//true
+console.log(regExp.test("ab"));//true
+
+var regExp = /a/gi;//思考如果加了g，循环了若干次后是true还是false，这是为什么？test中的lastIndex
+console.log(regExp.test("ab"));//true
+console.log(regExp.test("ab"));//false 为什么？
+console.log(regExp.test("ab"));//true
+console.log(regExp.test("ab"));//false 为什么？
+
+/*
+while (regExp.test("aaa")){
+    console.log(regExp.lastIndex);//每次执行后从哪开始重新匹配？
+}
+*/
